e305249e74c8250864237cb652a6445f73b6ce26
Nicolas ELIE <nicolaselie@free.fr>
2014-01-03 13:30:50 +0100

    Media files are only loaded if DEBUG is True.

--- cuisine/urls.py
+++ cuisine/urls.py
@@ -13,4 +13,6 @@ urlpatterns = patterns('',
     url(r'^', include('recipes.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
